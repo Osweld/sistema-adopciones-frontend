2ab49fdbe6f3c0fdc8b5420d503e89605c88e5af
FranGrande <fran.argrande@hotmail.com>
2023-06-01 21:59:16 -0600

    proceso de creacion de grupo de formulario

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as bootstrap from 'bootstrap';
+import { ActivatedRoute, Router } from '@angular/router';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public ingreso!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.ingreso= this.createMyForm();
+  }
+
+  private createMyForm():FormGroup{
+    return this.fb.group({
+      usuario:[],
+      password:[]
+    });
+  }
+
+  public submitFormulario(){
+    alert("Se va a enviar el formulario");
+    console.log(this.ingreso.value);
+    
   }
 
 }
+
+
