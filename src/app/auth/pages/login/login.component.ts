d541afe22609497c2b79198f42cf4f280684012f
FranGrande <fran.argrande@hotmail.com>
2023-09-26 17:30:57 -0600

    login con admin admin

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { __values } from 'tslib';
+import { UserService } from '../../services/user.service';
+import { Genero, Rol, User } from '../../interfaces/auth.interface';
+import { SharedService } from 'src/app/shared/Servicios/shared.service';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +14,44 @@ import { __values } from 'tslib';
 export class LoginComponent implements OnInit {
   public ingreso!: FormGroup;
   public ingresar: boolean = false;
+  usu1: string = '';
+  pas1: string = '';
+  msj: string = 'Ingrese sus credenciales';
+  valida: number = 0;
+  dataSource: User[] = [];
+  //user!: User;
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    public router:Router,
+    private userService:UserService,
+    private _sharedService: SharedService,
+    
   ) { }
 
   public ngOnInit(): void {
+    /*this.userService.getUserWithPagination(0).subscribe(page =>{
+      this.dataSource = page.content
+    })*/
+
     this.ingreso= this.createMyForm();
+  }
+
+  getValues(u:string, p:string,){
+    this.usu1 = u
+    this.pas1 = p
+    console.warn(this.pas1,"--",this.usu1)
+    if(this.usu1=="admin" && this.pas1=="admin" ){
+      this.valida = 1
+      this._sharedService.mostrarMensaje("green", "admitido", "Bienvenido")
+      this.go()
+    } else {
+      this.msj ="Las credenciales son incorrectas, vuelva a intentar"
+    }
+  }
+
+  go(){
+    this.router.navigate(['/Bienvenido'])
   }
 
   private createMyForm():FormGroup{
@@ -40,7 +75,6 @@ export class LoginComponent implements OnInit {
   public get f():any{
     return this.ingreso.controls;
   }
-
 
 
 }
