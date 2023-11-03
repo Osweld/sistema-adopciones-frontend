32dc86671578ba4e270ad3a662a3ce3148ae1345
German <oswaldo101.gr@gmail.com>
2023-11-03 11:08:55 -0600

    se modificio environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl:"https://sistema-adopcion-backend-production.up.railway.app"
+  baseUrl:"https://sistema-adopcion-backend-production.up.railway.app/"
 };
